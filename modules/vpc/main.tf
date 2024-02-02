a73971c9e3dfd29559c9fc0ccf723c653f39d2b7
satishreddy <satishreddysr777@gmail.com>
2024-02-01 21:39:07 -0500

    Terraform Code

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,3 +16,8 @@ resource "aws_subnet" "main" {
     Name = "subnet-${count.index}"
   }
 }
+
+resource "aws_vpc_peering_connection" "main" {
+  peer_vpc_id = aws_vpc.main.id
+  vpc_id      = data.aws_vpc.default.id
+}
