ed678062ecbc43806cf4222b62d806ffcdbbd63b
satishreddy <satishreddysr777@gmail.com>
2024-02-01 21:52:37 -0500

    Terraform Code

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -26,3 +26,15 @@ resource "aws_vpc_peering_connection" "main" {
     Name = "${var.env}-vpc-with-default-vpc"
   }
 }
+
+resource "aws_route" "main" {
+  route_table_id            = aws_vpc.main.main_route_table_id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+}
+
+resource "aws_route" "default-vpc" {
+  route_table_id            = aws_vpc.main.main_route_table_id
+  destination_cidr_block    = aws_vpc.main.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+}
