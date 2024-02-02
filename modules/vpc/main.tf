3eab707cc98f6b8e1ab53932472dee2e7edd386a
satishreddy <satishreddysr777@gmail.com>
2024-02-01 22:05:51 -0500

    Terraform Code

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -38,3 +38,45 @@ resource "aws_route" "default-vpc" {
   destination_cidr_block    = aws_vpc.main.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
 }
+
+
+
+// Testing purpose
+data "aws_ami" "centos8" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_security_group" "test" {
+  name        = "test"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "test-sg"
+  }
+}
+
+resource "aws_instance" "test" {
+  ami                    = data.aws_ami.centos8.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.main[0].id
+  vpc_security_group_ids = [aws_security_group.test.id]
+}
