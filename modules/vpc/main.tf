519be3b93953116c81408ce7a40d4dafe775a9dc
satishreddy <satishreddysr777@gmail.com>
2024-02-01 23:27:14 -0500

    Terraform Code

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -60,6 +60,20 @@ resource "aws_route_table_association" "public" {
   subnet_id         = lookup(element(aws_subnet.public, count.index), "id", null)
 }
 
+resource "aws_eip" "main" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "main" {
+  count             = length(var.public_subnets_cidr)
+  allocation_id     = lookup(element(aws_eip.main, count.index), "id", null)
+  subnet_id         = lookup(element(aws_subnet.public, count.index), "id", null)
+
+  tags = {
+    Name = "ngw-${count.index + 1}"
+  }
+}
+
 resource "aws_subnet" "private" {
   count             = length(var.private_subnets_cidr)
   vpc_id            = aws_vpc.main.id
@@ -70,6 +84,32 @@ resource "aws_subnet" "private" {
     Name = "subnet-private-${count.index + 1}"
   }
 }
+
+resource "aws_route_table" "private" {
+  count             = length(var.private_subnets_cidr)
+  vpc_id            = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = lookup(element(aws_nat_gateway.main, count.index), "id", null)
+  }
+
+  route {
+    cidr_block                = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+
+  tags = {
+    Name = "private-rt-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count             = length(var.private_subnets_cidr)
+  route_table_id    = lookup(element(aws_route_table.private, count.index), "id", null) # aws_route_table.private[count.index].id
+  subnet_id         = lookup(element(aws_subnet.private, count.index), "id", null)
+}
+
 
 
 
